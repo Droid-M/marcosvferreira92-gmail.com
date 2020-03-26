17cbb234be8b88ce8875accc4d69da278e4e8284
Marcos <Marcos@Marcos-PC>
2020-03-26 17:03:20 -0300

    Merge origin/master
    Conflicts:
    	src/DAO/GrafoDAO.java

--- src/Semente/GrafoAleatorio.java
+++ src/Semente/GrafoAleatorio.java
@@ -6,10 +6,16 @@ import java.util.Random;
 
 public class GrafoAleatorio {
 
+    private int n;
+
+    public GrafoAleatorio(int numeroVertices) {
+        this.n = numeroVertices;
+    }
+
     public Grafo geraGrafo() {
         Random random = new Random();
         Grafo grafo = new Grafo();
-        for (int i = 0; i < 500; i++) {
+        for (int i = 0; i < n; i++) {
             Vertice v1 = new Vertice(random.nextInt(9999999) + "");
             while (!grafo.adicionaVertice(v1)) {
                 v1 = new Vertice(random.nextInt(9999999) + "");
@@ -19,8 +25,10 @@ public class GrafoAleatorio {
             while (!grafo.adicionaVertice(v2)) {
                 v2 = new Vertice(random.nextInt(9999999) + "");
             }
-
-            grafo.insereLigacao(v1, v2, random.nextInt(1000));
+            int decisao = random.nextInt(2);
+            if (decisao < 1) {
+                grafo.insereLigacao(v1, v2, random.nextInt(1000));
+            }
         }
         return grafo;
     }
